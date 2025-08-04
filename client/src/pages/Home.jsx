4efd3290a2aeb88310bb7bec5154f469949dfc69
Chirag <rkchiragbhuyan@gmail.com>
2025-08-04 21:13:14 +0530

    Fix casing issue in Steps import

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Description from "../components/Description";
 import Header from "../components/Header";
-import Steps from "../components/steps";
+import Steps from "../components/Steps";
 import Testimonials from "../components/Testimonials";
 import GenerateBtn from "../components/GenerateBtn";
 
